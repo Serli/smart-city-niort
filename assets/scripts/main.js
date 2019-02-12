fc87959a848f2c36cdc015c6c5babbe6c01f7917
damienjoubert <damien.joubert@serli.com>
2019-02-12 15:47:11 +0100

    integration navbar3

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,21 @@
-console.log("Hello");
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems, {});
+    var element = document.querySelectorAll('.carousel');
+    M.Carousel.init(element, {
+        shift: 50
+    });
 });
+
+document.getElementById("button-navbar3").addEventListener("click", function () {
+    var elem = document.getElementById('carousel');
+    if(elem.style.display === "none") {
+        document.getElementById("navbar3").style.height = "164px";
+        elem.style.display = "block";
+    }
+    else {
+        elem.style.display = "none";
+        document.getElementById("navbar3").style.height = "30px";
+    }
+});
+
